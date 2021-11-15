34c0c7ae6fd34ba865f8bd196d19258aa259c82a
MohamadHujaij <64221174+MohamadHujaij@users.noreply.github.com>
2021-11-14 21:51:22 -0500

    L1.07: inverser le dictionnaire (fait)
    -L'ajout d'une clé à multiple valeurs cause un problème
    à régler.
    *À revoir

--- tile.py
+++ tile.py
@@ -23,48 +23,81 @@ class Tile:
                         TileType.WALL: (84, 62, 0),
                         TileType.EXIT: (64, 64, 64)}
 
-    TYPES_AND_SYMBOLS = {TileType.GROUND: ' ',
-                         TileType.STONE: 'S',
-                         TileType.WALL: 'W',
-                         TileType.NINJA_START_POS: 'N',
-                         TileType.SAMOURAI_START_POS_1: '1',
-                         TileType.SAMOURAI_START_POS_2: '2',
-                         TileType.SAMOURAI_START_POS_3: '3',
-                         TileType.SAMOURAI_START_POS_4: '4',
-                         TileType.SAMOURAI_START_POS_5: '5',
-                         TileType.SAMOURAI_START_POS_6: '6',
-                         TileType.EXIT: 'E'}
+    # TYPES_AND_SYMBOLS = {' ': {TileType : TileType.GROUND, 'walkable': True},
+    #                      'S': {TileType : TileType.STONE, 'walkable' : False},
+    #                      'W': {TileType: TileType.WALL, 'walkable' :False},
+    #                      'N': {TileType: TileType.NINJA_START_POS, 'walkable' :True},
+    #                      '1': {TileType: TileType.SAMOURAI_START_POS_1, 'walkable' :True},
+    #                      '2': {TileType: TileType.SAMOURAI_START_POS_2, 'walkable' :True},
+    #                      '3': {TileType: TileType.SAMOURAI_START_POS_3, 'walkable' :True},
+    #                      '4': {TileType: TileType.SAMOURAI_START_POS_4, 'walkable' :True},
+    #                      '5': {TileType: TileType.SAMOURAI_START_POS_5, 'walkable' :True},
+    #                      '6': {TileType: TileType.SAMOURAI_START_POS_6, 'walkable' :True},
+    #                      'E': {TileType: TileType.EXIT, 'walkable': True}}
+    TYPES_AND_SYMBOLS = {' ': TileType.GROUND,
+                         'S': TileType.STONE,
+                         'W': TileType.WALL,
+                         'N': TileType.NINJA_START_POS,
+                         '1': TileType.SAMOURAI_START_POS_1,
+                         '2': TileType.SAMOURAI_START_POS_2,
+                         '3': TileType.SAMOURAI_START_POS_3, 
+                         '4': TileType.SAMOURAI_START_POS_4, 
+                         '5': TileType.SAMOURAI_START_POS_5,
+                         '6': TileType.SAMOURAI_START_POS_6,
+                         'E': TileType.EXIT}
+
 
     def __init__(self, walkable: bool = True, tile_type: TileType = TileType.GROUND) -> None:
         self.__walkable = walkable
         self.__tile_type = tile_type
 
     def __str__(self) -> str:
-        return self.TYPES_AND_SYMBOLS[self.__tile_type]
+        #return self.TYPES_AND_SYMBOLS[self.__tile_type]
+        return str(list(self.TYPES_AND_SYMBOLS.keys())[list(self.TYPES_AND_SYMBOLS.values()).index(self.__tile_type)])
 
     @staticmethod
     def create_from_symbol(symbol: str):
         """Crée et configure une tuile en fonction du symbole."""
-        if symbol == Tile.TYPES_AND_SYMBOLS[TileType.STONE]:
-            return Tile(tile_type=TileType.STONE, walkable=False)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.WALL]:
-            return Tile(tile_type=TileType.WALL, walkable=False)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.NINJA_START_POS]:
-            return Tile(tile_type=TileType.NINJA_START_POS)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_1]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_1)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_2]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_2)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_3]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_3)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_4]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_4)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_5]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_5)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.SAMOURAI_START_POS_6]:
-            return Tile(tile_type=TileType.SAMOURAI_START_POS_6)
-        elif symbol == Tile.TYPES_AND_SYMBOLS[TileType.EXIT]:
-            return Tile(tile_type=TileType.EXIT)
+        if symbol == 'S':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['S'][TileType],
+            #  walkable=Tile.TYPES_AND_SYMBOLS['S']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['S'], walkable=False)
+        elif symbol == 'W':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['W'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['W']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['W'], walkable=False)
+        elif symbol == 'N':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['N'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['N']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['N'])
+        elif symbol == '1':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['1'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['1']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['1'])
+        elif symbol == '2':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['2'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['2']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['2'])
+        elif symbol == '3':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['3'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['3']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['3'])
+        elif symbol == '4':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['4'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['4']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['4'])
+        elif symbol == '5':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['5'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['5']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['5'])
+        elif symbol == '6':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['6'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['6']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['6'])
+        elif symbol == 'E':
+            # return Tile(tile_type= Tile.TYPES_AND_SYMBOLS['E'][TileType], 
+            # walkable=Tile.TYPES_AND_SYMBOLS['E']['walkable'])
+            return Tile(tile_type=Tile.TYPES_AND_SYMBOLS['E'])
         else:
             return Tile()
 
